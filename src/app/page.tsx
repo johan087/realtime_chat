dd54191281819a1e420bc4d0c4e1b942774243bc
johanrussouw <jrussouw610505@gmail.com>
2026-01-16 11:08:02 +0200

    create secure room implemented

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { useMutation } from "@tanstack/react-query";
 import { nanoid } from "nanoid";
 import { useEffect, useState } from "react";
 import { client } from "./lib/client";
+import { useRouter } from "next/navigation";
 
 const ANIMALS = ["wolf", "hawk", "bear", "shark"];
 
@@ -15,6 +16,7 @@ const generateUsername = () => {
 
 export default function Home() {
   const [username, setUsername] = useState("");
+  const router = useRouter();
 
   useEffect(() => {
     const main = () => {
@@ -36,7 +38,7 @@ export default function Home() {
   const { mutate: createRoom } = useMutation({
     mutationFn: async () => {
       const res = await client.room.create.post({});
-      return res;
+      if (res.status === 200) router.push(`/room/${res.data?.roomId}`);
     },
   });
 
