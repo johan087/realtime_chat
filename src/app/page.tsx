506bb918589046bc6d818b494e5c53e5adf61c60
johanrussouw <jrussouw610505@gmail.com>
2026-01-16 09:50:22 +0200

    added upstash/redis

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,6 +36,7 @@ export default function Home() {
   const { mutate: createRoom } = useMutation({
     mutationFn: async () => {
       const res = await client.room.create.post({});
+      return res;
     },
   });
 
@@ -62,7 +63,10 @@ export default function Home() {
                 </div>
               </div>
             </div>
-            <button className="w-full bg-zinc-100 text-black p-3 text-sm font-bold hover:bg-zinc-50 hover:text-black transition-colors mt-2 cursor-pointer disabled:opacity-50">
+            <button
+              onClick={() => createRoom()}
+              className="w-full bg-zinc-100 text-black p-3 text-sm font-bold hover:bg-zinc-50 hover:text-black transition-colors mt-2 cursor-pointer disabled:opacity-50"
+            >
               CREATE SECURE ROOM
             </button>
           </div>
