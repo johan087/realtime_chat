441015ae6b55f8148a1f4cbcab2605a47d8b5b80
johanrussouw <jrussouw610505@gmail.com>
2026-01-16 17:00:55 +0200

    rooom header complete

--- src/app/room/[roomId]/page.tsx
+++ src/app/room/[roomId]/page.tsx
@@ -3,11 +3,18 @@
 import { useParams } from "next/navigation";
 import { useState } from "react";
 
+function formatTimeReaning(seconds: number) {
+  const mins = Math.floor(seconds / 60);
+  const secs = seconds % 60;
+  return `${mins}:${secs.toString().padStart(2, "0")}`;
+}
+
 const Page = () => {
   const params = useParams();
   const roomId = params.roomId as string;
 
   const [copyStatus, setCopyStatus] = useState("COPY");
+  const [timeRemainig, setTimeRemainig] = useState<number | null>(null);
 
   const copyLink = () => {
     const url = window.location.href;
@@ -18,7 +25,7 @@ const Page = () => {
 
   return (
     <main className="flex flex-col h-screen max-h-screen overflow-hidden ">
-      <header className="border-b border-zinc-800 p-4 flex items-center justify-between bg-zinc-900/30">
+      <header className="border-b bg-black border-zinc-800 p-4 flex items-center justify-between ">
         <div className="flex items-center gap-4">
           <div className="flex flex-col">
             <span className="text-xs text-zinc-500 uppercase">Room ID </span>
@@ -32,7 +39,28 @@ const Page = () => {
               </button>
             </div>
           </div>
+          <div className="h-8 w-px bg-zinc-800" />
+          <div className="flex flex-col">
+            <span className="text-xs text-zinc-500 uppercase">
+              self-destruct
+            </span>
+            <span
+              className={`text-sm font-bold flex items-center gap-2 ${
+                timeRemainig !== null && timeRemainig < 60
+                  ? "text-red-500"
+                  : "text-amber-500"
+              }`}
+            >
+              {timeRemainig !== null
+                ? formatTimeReaning(timeRemainig)
+                : "--:--"}
+            </span>
+          </div>
         </div>
+        <button className="text-xs bg-zinc-800 hover:bg-red-600 px-3 py-1.5 rounded text-zinc-400 hover:text-white font-bold transition-all group flex items-center  gap-2 disabled:opacity-50">
+          <span className="group-hover:animate-pulse">💣</span>
+          DESTROY NOW
+        </button>
       </header>
     </main>
   );
